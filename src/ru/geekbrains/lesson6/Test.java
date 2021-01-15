cdb0f8c08031cceb3fb67ba013127a2d4903e4bf
aabasalov <aabasalov@yandex.ru>
2021-01-15 12:18:08 +0300

    Lesson6

--- src/ru/geekbrains/lesson6/Test.java
+++ src/ru/geekbrains/lesson6/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Animal cat = new Cat("Remus", 3, "White", 200, 0, 2);
         Animal dog = new Dog("Shuma",4 , "Black-white", 500, 10, 0.5);
-        Animal cat1 = new Cat("Pushistik", 5, "Grey", 200, 0, 2);
+        Animal cat1 = new Cat("Pushistik", 6, "Grey", 200, 0, 2);
 
         cat1.jump(5);
         cat.swim(400);
